23e01ccf0b742f765e70652c3ba67381807cc879
anesha <anesha.s.jones@gmail.com>
2016-10-19 16:32:09 -0400

    first commit

---
+++ tipcalculator.js
@@ -0,0 +1,5 @@
+
+var bill_total= parseFloat(prompt ('What is your bill amount?'));
+var tip_amount= (.20)* bill_total;
+var total_amount= (bill_total + tip_amount);
+alert(total_amount + " is your total bill plus tip");
